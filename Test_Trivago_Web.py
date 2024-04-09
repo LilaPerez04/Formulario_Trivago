8e1730d7d0a5378f5dbcd2b4c8bde060efc40bc1
Liliana Pérez Herrera <114111230+LilaPerez04@users.noreply.github.com>
2024-04-08 21:53:41 -0600

    Update Test_Trivago_Web.py

--- Test_Trivago_Web.py
+++ Test_Trivago_Web.py
@@ -1,25 +1,39 @@
 # Configuramos nuestras librerías
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import time
 
 # Configuramos los drivers para Chrome y especificamos la URL de Trivago
 driver = webdriver.Chrome()
 driver.get("https://www.trivago.com.mx/es-MX")
 
-# Configuramos la clase
+# Vamos a clickear botones para probar localizadores
+destiny_city_searcher = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'input-auto-complete')))
+destiny_city_searcher.click()
+destiny_city_searcher.send_keys("Valentin Imperial Riviera Maya")
 
-class Trivago:
+time.sleep(3)
+
+#quit_destiny = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(((By.CLASS_NAME, 'Icon_wrapper__B6IoS Icon_s__HT6ei'))))
+#quit_destiny.click()
+
+#destiny_city_text = (By.CLASS_NAME, 'text-m leading-normal font-bold truncate text-grey-900')
+#time.sleep(3)
+
+driver.quit()
+
+#class Trivago:
 
     #Localizadores
 
-    destiny_city_searcher = (By.CLASS_NAME, 'AutoComplete_searchInput__Km3dQ AutoComplete_inputDropdownContainer__FsmFI')
-
-    destiny_city_text = (By.CLASS_NAME, 'text-m leading-normal font-bold truncate text-grey-900')
-    quit_current_destiny = (By.CLASS_NAME, 'Icon_wrapper__B6IoS Icon_s__HT6ei')
-
-    calendar = (By.CLASS_NAME, 'flex items-center 2xl:p-2 2xl:hover:bg-grey-200 2xl:rounded-md group-focus:ring-2 group-focus:ring-blue-700 group-focus:ring-inset')
+#    destiny_city_searcher = (By.ID, 'input-auto-complete')
+#    destiny_city_text = (By.CLASS_NAME, 'text-m leading-normal font-bold truncate text-grey-900')
+#    quit_current_destiny = (By.CLASS_NAME, 'Icon_wrapper__B6IoS Icon_s__HT6ei')
 
     #Driver
-    def __init__(self, driver):
-        self.driver = driver
+#    def __init__(self, driver):
+#        self.driver = driver
+
 
