ff69e4c8fed98f954be7567d744a50f986ba38c3
Esparta <damelisquerales@gmail.com>
2024-04-11 20:42:58 +0200

    -Se incluye maximize window -Se incluye función para "aceptar" selección de huespedes

--- Test_Trivago_Web2.py
+++ Test_Trivago_Web2.py
@@ -9,17 +9,17 @@ import time
 driver = webdriver.Chrome()
 url = "https://www.trivago.com.mx/es-MX"
 driver.get(url)
-
+driver.maximize_window()
 
 # Interacción con Elementos de la Página:
 
-    # Seleccionar destino
+# Seleccionar destino
 destiny_city_searcher = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.ID, 'input-auto-complete')))
 destiny_city_searcher.click()
 destiny_city_searcher.send_keys("Valentin Imperial Riviera Maya")
 time.sleep(3)
 
-    # Seleccionar calendario
+# Seleccionar calendario
 arrival_departure_calendar = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.XPATH, '(//*[@id="__next"]/div[1]/div[2]/section[1]/div[2]/div[4]/div/div[1]/fieldset/button[1])[1]')))
 arrival_departure_calendar.click()
 time.sleep(3)
@@ -65,5 +65,34 @@ def set_calendar(month_year, year, month_number, day):
 set_calendar(check_in["month_year_ci"], check_in["year_ci"], check_in["month_number_ci"], check_in["day_ci"])
 set_calendar(check_out["month_year_co"], check_out["year_co"], check_out["month_number_co"], check_out["day_co"])
 time.sleep(3)
+
+
+# Seleccionar huespedes
+guest_counter_plus = WebDriverWait(driver, 5).until(ec.element_to_be_clickable(
+    (By.XPATH, "//button[contains(@data-testid,'guest-selector-apply')]")))
+guest_counter_plus.click()
+time.sleep(2)
+
+
+def guest_counter_plus():
+    for _ in range(2):
+        driver.find_element(guest_counter_plus)
+        time.sleep(3)
+
+
+guest_counter_less = By.XPATH, "//span[contains(@class,'Icon_wrapper__B6IoS Icon_centered__WVTjD Icon_full__sdZ0N')])[2]"
+
+
+def guest_counter():
+    for _ in range(3):
+        driver.find_element(guest_counter_less)
+        time.sleep(3)
+
+
+guest_counter_confirm = WebDriverWait(driver, 5).until(ec.element_to_be_clickable(
+    (By.XPATH, "//button[@data-testid='guest-selector-apply']")))
+guest_counter_confirm.click()
+time.sleep(2)
+
 
 driver.quit()
