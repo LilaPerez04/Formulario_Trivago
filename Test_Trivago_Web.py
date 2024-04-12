02a9633ac6a18e9aa3874c7feedc6b17c4af8b3b
Gallelito <mezaolivasjavier50@gmail.com>
2024-04-11 20:01:38 -0700

    Primer path para seleccionar una fecha de salida deseada

--- Test_Trivago_Web.py
+++ Test_Trivago_Web.py
@@ -23,8 +23,10 @@ destiny_city_searcher.send_keys("Valentin Imperial Riviera Maya")
 time.sleep(3)
 
 
-def pick_a_arrival_date(self, desired_date):
+def pick_a_arrival_date(self):
 
+    checkin_date = "2024-05-10"
+    checkout_date = "2024-06-12"
     arrival_calendar = (
     By.XPATH, '//*[@id="__next"]/div[1]/div[2]/section[1]/div[2]/div[4]/div/div/fieldset/button[1]/span')
 
@@ -34,13 +36,15 @@ def pick_a_arrival_date(self, desired_date):
     # Establecer un límite para buscar la fecha deseada (hasta un año)
     month_count = 0
     while month_count < 12:
+
+        ########### Fecha de salida ########
         try:
             # Buscar el botón de la fecha deseada dentro del calendario actual
-            date_button = self.driver.find_element(By.XPATH,
-                                                   f"//button[@data-testid='valid-calendar-day-{desired_date}']")
+            checkin_date_button = self.driver.find_element(By.XPATH,
+                                                   f"//button[@data-testid='valid-calendar-day-{checkin_date}']")
 
-            if date_button.is_displayed():
-                date_button.click()
+            if checkin_date_button.is_displayed():
+                checkin_date_button.click()
                 return  # Salir del método una vez que la fecha deseada sea seleccionada
         except:
             pass  # Continuar con el siguiente mes si la fecha no es encontrada
@@ -48,7 +52,21 @@ def pick_a_arrival_date(self, desired_date):
         # Hacer clic en el botón siguiente para avanzar al siguiente mes
         next_button = self.driver.find_element(By.XPATH, "//button[@data-testid='calendar-button-next']")
         next_button.click()
-        time.sleep(1)  # Esperar brevemente después de hacer clic en el botón siguiente
+        time.sleep(2)  # Esperar brevemente después de hacer clic en el botón siguiente
+
+        ######### Fecha de regreso ###################
+        try:
+            # Buscar el botón de la fecha deseada dentro del calendario actual
+            checkout_date_button = self.driver.find_element(By.XPATH,
+                                                   f"//button[@data-testid='valid-calendar-day-{checkout_date}']")
+            checkout_date_button.click()
+        except:
+            pass  # Continuar con el siguiente mes si la fecha no es encontrada
+
+            # Hacer clic en el botón siguiente para avanzar al siguiente mes
+            next_button = self.driver.find_element(By.XPATH, "//button[@data-testid='calendar-button-next']")
+            next_button.click()
+            time.sleep(1)  # Esperar brevemente después de hacer clic en el botón siguiente
 
         month_count +=1
 
