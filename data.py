e853f2979608876dcad7c5eeb6419a0a3b102eb7
JavierMeO <150411748+JavierMeO@users.noreply.github.com>
2024-04-18 13:53:57 -0700

    Update data.py

--- data.py
+++ data.py
@@ -9,8 +9,8 @@ hotel = "Ocean Coral & Turquesa"
 # Establecer la fecha de check in
 check_in = {
     "day_ci": '10',
-    "month_ci": 'Junio',
-    "month_number_ci": '06',
+    "month_ci": 'Mayo',
+    "month_number_ci": '05',
     "year_ci": '2024',
 }
 # Agregando un nuevo elemento al diccionario check_in (concatenando mes y año)
@@ -18,10 +18,9 @@ check_in["month_year_ci"] = check_in["month_ci"] + " " + check_in["year_ci"]
 
 # Establecer la fecha de check out
 check_out = {
-    "day_co": '10',
+    "day_co": '28',
     "month_co": 'Julio',
     "month_number_co": '07',
     "year_co": '2024',
 }
 # Agregando un nuevo elemento al diccionario check_out (concatenando mes y año)
-check_out["month_year_co"] = check_out["month_co"] + " " + check_out["year_co"]
