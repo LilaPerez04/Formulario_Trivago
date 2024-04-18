e816656b14cd1772f59d8e6f9a8daecea476c0a4
Liliana Pérez Herrera <114111230+LilaPerez04@users.noreply.github.com>
2024-04-18 12:48:54 -0600

    Update Locators.py

--- Locators.py
+++ Locators.py
@@ -1,11 +1,23 @@
 from selenium.webdriver.common.by import By
+import data
 
-# Elementos de la Página
+# Elementos de la página Trivago
 
+# Campo de destino
 destiny_city_searcher = (By.ID, 'input-auto-complete')
-arrival_departure_calendar = (By.XPATH, '(//*[@id="__next"]/div[1]/div[2]/section[1]/div[2]/div[4]/div/div[1]/fieldset/button[1])[1]')
-displayed_month_year = (By.XPATH, '//div[@data-testid="calendar-popover"]//*[contains(@class, "text-center")]//h3[contains(@class, "Heading_heading__xct3h")][1]')
-next_button = (By.XPATH, '//button[@data-testid="calendar-button-next"]')
+
+# Calendario
+arrival_departure_calendar = (By.XPATH,
+                              '(//*[@id="__next"]/div[1]/div[2]/section[1]/div[2]/div[4]/div/div[1]/fieldset/button[1])'
+                              '[1]')
+displayed_month_year = (By.XPATH,
+                        '//div[@data-testid="calendar-popover"]//*[contains(@class, "text-center")]'
+                        '//h3[contains(@class, "Heading_heading__xct3h")][1]')
+next_button = (By.XPATH,
+               '//button[@data-testid="calendar-button-next"]')
+correct_day = (By.XPATH, '//button[@data-testid="valid-calendar-day-{year}-{month_number}-{day}"]')
+
+# Menú habitaciones y huéspedes 
 guests_and_rooms_button = (By.XPATH, "//span[@data-testid='search-form-guest-selector-value']")
 adults_plus_counter = (By.XPATH, "//button[@data-testid='adults-amount-plus-button']//span[1]")
 adults_minus_counter = (By.XPATH, "//button[@data-testid='adults-amount-minus-button']//span[1]")
@@ -19,4 +31,5 @@ pets_allowed_checkbox = (By.XPATH, "//input[@data-testid='pet-friendly-filter']"
 restart_guests_view = (By.CLASS_NAME, 'FlyoutGuestsRooms_resetBtn__1oUka')
 accept_guests_and_rooms_button = (By.XPATH, "//button[text()='Aceptar']")
 click_on_search_button = (By.XPATH, "//span[text()='Buscar']")
-hostel_card = (By.XPATH, "//div[@id='__next']/div[1]/main[1]/div[3]/section[1]/div[1]/div[1]/ol[1]/li[6]/div[1]/article[1]/div[2]/div[1]")
+hostel_card = (By.XPATH, "//div[@id='__next']/div[1]/main[1]/div[3]/section[1]/div[1]/div[1]/ol[1]/li[6]/div[1]"
+                         "/article[1]/div[2]/div[1]")
