a18afd4467569da158a8fdf6ecf8b98e346a518f
Gallelito <mezaolivasjavier50@gmail.com>
2024-04-18 19:32:40 -0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Test_Trivago_Web2.py

--- Locators.py
+++ Locators.py
@@ -35,4 +35,4 @@ click_on_search_button = (By.XPATH, "//span[text()='Buscar']")
 hostel_card = (By.XPATH, "//div[@id='__next']/div[1]/main[1]/div[3]/section[1]/div[1]/div[1]/ol[1]/li[6]/div[1]"
                          "/article[1]/div[2]/div[1]")
 
-#1
+#13
